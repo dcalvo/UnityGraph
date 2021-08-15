c5549778103eead8aceaf3d9db48bc1b08de0802
David Calvo <dcalvo2@jhu.edu>
2021-08-15 02:07:11 -0400

    Mathematical Surfaces

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -9,26 +9,32 @@ public class Graph : MonoBehaviour {
 
     private void Awake() {
         float step = 2f / resolution;
-        Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step;
 
-        points = new Transform[resolution];
-        for (int i = 0; i < points.Length; i++) {
-            Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
-            point.localPosition = position;
+        points = new Transform[resolution * resolution];
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            Transform point = Instantiate(pointPrefab);
+
             point.localScale = scale;
             point.SetParent(transform, false);
+            points[i] = point;
         }
     }
 
     private void Update() {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        foreach (Transform point in points) {
-            Vector3 position = point.localPosition;
-            position.y = f(position.x, time);
-            point.localPosition = position;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+
+            float u = (x + 0.5f) * step - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
